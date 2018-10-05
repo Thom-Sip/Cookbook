1eec71ddd3064b8887314f3d8a8715f768a82144
Thom Sip <35060642+Thom-Sip@users.noreply.github.com>
2018-10-05 16:40:08 +0200

    Extending the Editor (Count children)

---
+++ Cookbook/Commands/GetChildCount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+
+namespace Cookbook.Commands
+{
+    public class GetChildCount : Sitecore.Shell.Framework.Commands.Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            if(context.Items.Length == 1)
+            {
+                Item currentItem = context.Items[0];
+                SheerResponse.Alert($"Children count: {currentItem.Children.Count}");
+            }
+        }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            if(context.Items.Length == 1)
+            {
+                Item currentItem = context.Items[0];
+                if(currentItem.Children.Count == 0)
+                {
+                    return CommandState.Hidden;
+                }
+            }
+            return base.QueryState(context);
+        }
+    }
+}
