26e9fd8b23166d6516a29d06a35714110a93cf4a
Thom Sip <35060642+Thom-Sip@users.noreply.github.com>
2018-10-05 09:21:03 +0200

    Add carousel

---
+++ Cookbook/Controllers/NavigationController.cs
@@ -0,0 +1,31 @@
+﻿using Cookbook.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookbook.Controllers
+{
+    public class NavigationController : Controller
+    {
+        public ActionResult Carousel()
+        {
+            List<CarouselSlide> slides = new List<CarouselSlide>();
+            MultilistField multilistField = Sitecore.Context.Item.Fields["Carousel Slides"];
+
+            if(multilistField != null)
+            {
+                Item[] carouselItems = multilistField.GetItems();
+                foreach(var item in carouselItems)
+                {
+                    slides.Add(new CarouselSlide(item));
+                }
+            }
+
+            return PartialView(slides);
+        }
+    }
+}
--- Cookbook/Helpers/SiteHelper.cs
+++ Cookbook/Helpers/SiteHelper.cs
@@ -1,4 +1,6 @@
-﻿using Sitecore.Data.Items;
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,31 @@ namespace Cookbook.Helpers
         {
             string homePath = Sitecore.Context.Site.StartPath;
             return  Sitecore.Context.Database.GetItem(homePath);           
+        }
+
+        public static string GetImageUrl(Item item)
+        {
+            //var currentItem = Sitecore.Context.Item;
+            var currentItem = item;
+            var imageUrl = string.Empty;
+
+            Sitecore.Data.Fields.ImageField imageField = currentItem.Fields["Image"];
+            if (imageField?.MediaItem != null)
+            {
+                var image = new MediaItem(imageField.MediaItem);
+                imageUrl = StringUtil.EnsurePrefix('/', MediaManager.GetMediaUrl(image));
+            }
+
+            return imageUrl;
+        }
+
+        public static string GetImageUrlFromHtmlString(HtmlString htmlString)
+        {
+            string url = htmlString.ToString();
+            var start = url.IndexOf('"', 0);
+            var end = url.IndexOf('"', start + 1);
+            var newurl = url.Substring(start, end - start);
+            return url.Substring(start, end - start);
         }
     }
 }
