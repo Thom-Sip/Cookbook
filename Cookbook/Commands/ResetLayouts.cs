cb7d4637393a96e8319c3cc28ac8c5c942aa67ca
Thom Sip <35060642+Thom-Sip@users.noreply.github.com>
2018-10-08 13:32:42 +0200

    Reset LAyouts

---
+++ Cookbook/Commands/ResetLayouts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cookbook.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Cookbook.Commands
+{
+    public class ResetLayouts : Sitecore.Shell.Framework.Commands.Command
+    {
+        
+        public override void Execute(CommandContext context)
+        {
+            ResetLayoutDetailsJob job = new ResetLayoutDetailsJob();
+            Item item = Sitecore.Configuration.Factory.GetDatabase("master").GetItem("/sitecore/Content/Home");
+            job.Run(item);
+        }
+    }
+}
