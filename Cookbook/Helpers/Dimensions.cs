b8fd89e6aa45ba8218148524c0c2974c19e5cb7e
Thom Sip <35060642+Thom-Sip@users.noreply.github.com>
2018-10-09 14:14:53 +0200

    Responsive Images

---
+++ Cookbook/Helpers/Dimensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cookbook.Helpers
+{
+    public class Dimensions
+    {
+        public int ScreenSize { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Dimensions(int screenSize, int width, int height)
+        {
+            ScreenSize = screenSize;
+            Width = width;
+            Height = height;
+        }
+    }
+}
