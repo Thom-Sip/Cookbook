6509176b0554596b138e63d5769e203075d132f7
Thom Sip <35060642+Thom-Sip@users.noreply.github.com>
2018-10-10 12:01:05 +0200

    Progress on Search function using Helix

---
+++ Cookbook/Features/Search/SearchController.cs
@@ -0,0 +1,53 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.SearchTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace Cookbook.Features.Search
+{
+    public class SearchController : Controller
+    { 
+        public ActionResult SearchForm()
+        {
+            // var viewLocation = HostingEnvironment.MapPath("~/Features/Search/SearchForm.cshtml");
+
+            //return PartialView("~/Features/Search/test.cshtml"); //, new SearchFormModel());
+
+            return PartialView(new SearchFormModel());
+
+            // return Content("Hello");
+        }
+
+        public List<SearchResultItem> SearchBook(string str, string orderBy, int pageSize, int pageNo, out int totalResults)
+        {
+            string index = $"sitecore_{Sitecore.Context.Database.Name}_index";
+
+            using (var context = ContentSearchManager.GetIndex(index).CreateSearchContext())
+            {
+                var query = context.GetQueryable<SearchResultItem>()
+                                .Where(p => p.Path.StartsWith("/sitecore/Content/home"))
+                                .Where(p => p.TemplateName == "book")
+                                .Where(p => p.Name.Contains(str));
+
+                totalResults = query.Count();
+
+                if(!string.IsNullOrWhiteSpace(orderBy))
+                {
+                    if (orderBy == "name")
+                        query = query.OrderBy(p => p.Name);
+
+                    else if (orderBy == "date")
+                        query = query.OrderBy(p => p.Updated);
+                }
+
+                query = query.Page(pageNo - 1, pageSize);
+                return query.ToList();
+            }
+        }
+    }
+}
---
+++ Cookbook/Features/Search/SearchFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cookbook.Features.Search
+{
+    public class SearchFormModel
+    {
+        public string SearchTerm { get; set; }
+
+        public string Sorting { get; set; }
+    } 
+}
